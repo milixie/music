287d0ff8dab6d2761dc53dfae2d6d2ee58c3eede
xiemeili <xiemeili@luojilab.com>
2018-01-13 11:16:52 +0800

    create base xiaocheng project

---
+++ client/app.js
@@ -0,0 +1,31 @@
+const regeneratorRuntime = require('./utils/regenerate');
+const Promise = require('./utils/promise');
+App({
+  /**
+   * Global shared
+   * 可以定义任何成员，用于在整个应用中共享
+   */
+  data: {
+    name: 'music',
+    version: '0.0.1',
+  },
+
+  async onLaunch() {
+  try {
+  } catch (e) {
+
+  }
+},
+/**
+ * 生命周期函数--监听小程序显示
+ * 当小程序启动，或从后台进入前台显示，会触发 onShow
+ */
+async onShow() {
+},
+/**
+ * 生命周期函数--监听小程序隐藏
+ * 当小程序从前台进入后台，会触发 onHide
+ */
+onHide() {
+}
+});
---
+++ client/pages/test/test.js
@@ -0,0 +1,13 @@
+'use strict';
+const regeneratorRuntime = require('../../utils/regenerate');
+const Promise = require('../../utils/promise');
+
+Page({
+  data: {
+
+  },
+  async onLoad() {
+    console.log('page is loading')
+  }
+});
+
